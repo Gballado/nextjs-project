ecfec2e0db09be712374fd8e005322df63feca7e
Gballado <95064164+Gballado@users.noreply.github.com>
2023-05-26 09:40:07 -0500

    Pages and Routing
    Learned how to navigate using routing and file based pages through making a simple website.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
+import EventList from "@/components/events/event-list";
+import { getFeaturedEvents } from "@/dummy-data";
+import EventsSearch from "@/components/events/events-search";
+import { Fragment } from "react";
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
-    <div>
-    </div>
-  )
+    <Fragment>
+      <EventsSearch/ >
+      <EventList items={featuredEvents} />
+    </Fragment>
+  );
 }
+
+export default HomePage;
